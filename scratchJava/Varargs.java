1ce9b958ca93c28afd1581d50dac3ca6dbeaca8e
Pavithran030 <techpavithran18@gmail.com>
2025-07-15 13:44:43 +0530

    15-07-2025 Update

---
+++ scratchJava/Varargs.java
@@ -0,0 +1,27 @@
+class Names{
+    public static void man(int... n){
+        for(int b:n){
+            System.out.print(b+" ");
+        }
+        System.out.println();
+    }
+}
+class Unary{
+    public static void uni(){
+        int a=10;
+        int b=20;
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(b--);
+        System.out.println(--b);
+    }
+}
+public class Varargs {
+
+    
+    public static void main(String[] args) {
+        // Names st=new Names();
+        Names.man(1,2,3,4,5,6,7,8,9,0,10);
+        Unary.uni();
+    }
+}

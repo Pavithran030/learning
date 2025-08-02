f9715073fa48c7b33a242bea10ec6f15aa48b390
Pavithran030 <techpavithran18@gmail.com>
2025-08-02 23:41:40 +0530

    02-08-2025 Update

---
+++ java/Collections_Map.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class Collections_Map {
+    public static void main(String[] args) {
+        Map <String,Integer> maps =new HashMap <String,Integer>();
+        maps.put("Monday-stock update", 35);
+        maps.put("Tuesday-stock update",10);
+        System.out.println(maps);
+    }
+    
+}

ef4f5677a3b25cadbcdea74bb428d6817066f3b9
Pavithran030 <techpavithran18@gmail.com>
2025-08-04 23:10:16 +0530

    Error solved 04-08-2025

--- java/Collections_Map.java
+++ java/Collections_Map.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Collections_Map {
     public static void main(String[] args) {
-        Map <String,Integer> maps =new HashMap <String,Integer>();
+        Map <String,Integer> maps =new HashMap <>();
         maps.put("Monday-stock update", 35);
         maps.put("Tuesday-stock update",10);
         System.out.println(maps);

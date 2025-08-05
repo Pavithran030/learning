b2117e9635f95ab0a94c539259d9700c837a7900
Pavithran030 <techpavithran18@gmail.com>
2025-08-05 22:02:57 +0530

    feat: added on 05-08-2025

--- java/Collections_Map.java
+++ java/Collections_Map.java
@@ -4,9 +4,8 @@ import java.util.*;
 public class Collections_Map {
     public static void main(String[] args) {
         Map <String,Integer> maps =new HashMap <>();
-        maps.put("Monday-stock update", 35);
-        maps.put("Tuesday-stock update",10);
-        System.out.println(maps);
+        maps.put("Monday_stock", 35);
+        maps.put("Tuesday_stock",10);
+        System.out.println(maps.keySet());
     }
-    
 }

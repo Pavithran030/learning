90e5e4adde33b896e7bdeead1d99596acc9141fb
Pavithran G <147511371+Pavithran030@users.noreply.github.com>
2025-08-06 23:27:30 +0530

    06-08-2025 Update

---
+++ java/Foreach_loop.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class Foreach_loop {
+
+        public static void main(String[] args) {
+        List <Integer> lst=Arrays.asList(1,2,3,4,5,6);
+
+        lst.forEach(n -> System.out.println(n));
+    }
+}
+
